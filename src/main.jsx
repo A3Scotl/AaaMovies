8930c13322a14e4a17ba9c056529758bd4e72c68
Nguyen Truong An <nguyentruongan0610@gmail.com>
2025-05-08 22:46:44 +0700

    ADD : Config router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Movies from './pages/Movies.jsx'
+import Auth from './pages/Auth.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: '/movies',
+        element: <Movies />
+      },
+      {
+        path: '/auth',
+        element: <Auth />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
