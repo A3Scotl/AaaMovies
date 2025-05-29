74c5c64f42ece27340cd92ca8303f0f11bff24a6
Nguyen Truong An <nguyentruongan0610@gmail.com>
2025-05-29 12:34:15 +0700

    FIX: LoadingSpinner component

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -6,7 +6,6 @@ const LoadingSpinner = () => {
             <div className="absolute top-0 left-0 w-16 h-16 border-4 border-transparent border-t-red-500 rounded-full animate-spin"></div>
           </div>
         </div>
-        <p className="text-white text-xl font-medium">Loading ...</p>
       </div>
   );
 };
